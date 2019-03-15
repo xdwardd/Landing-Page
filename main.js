bdf1a5c5509bc8240658ed7ac3372faa34763ffb
Edward Catapan <xdwardd@gmail.com>
2019-03-15 18:45:00 +0800

    Innitial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+//animate smooth scroll
+
+$('#view-work').on('click', function () {
+    const images = $('#images').position().top;
+
+    $('html, body').animate(
+        {
+                scrollTop: images
+        },
+            900
+        );
+    
+});
